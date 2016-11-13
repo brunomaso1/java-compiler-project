17c9fba16b6e03f1893a3cbd9890bc60193d86b5
mauri <mauricio.garcia@dixtra.co>
2016-11-13 10:56:05 -0300

    adding testcases

--- src/main/java/TestCases/TestCases.java
+++ src/main/java/TestCases/TestCases.java
@@ -10,8 +10,11 @@ import parser.*;
 public class TestCases {
 
 	private static String VARIABLE_A_TESTEAR = "$variable";
+	private static String VARIABLE_A_TESTEAR_SEGUNDA = "$varible2";
 	private static Expresion NUMERAL_A_TESTEAR = new Numeral(5.0);
+	private static Expresion NUMERAL_A_TESTEAR_SEGUNDA = new Numeral(6.0);
 	private static Expresion BOOLEAN_VERDADERO_A_TESTEAR = new ValorVerdad(true);
+	private static Expresion BOOLEAN_FALSO_A_TESTEAR = new ValorVerdad(false);
 	
 	@Test
 	public void testDeclaracionIguales() throws Exception {
@@ -67,18 +70,11 @@ public class TestCases {
 	}	
 	
 	@Test
-	public void test5() throws Exception {
-		System.out.println("poner esVerdadero en boolean $variable. distinto a  poner esVerdadero en boolean $variable2.");
+	public void inicializarVariable() throws Exception {
+		System.out.println("crearVariable $a tipo entero.");
 		
 //		crearVariable $a tipo entero.
-//
-//		=============
-//		=============
 //		key :$a (tipo:ENTERO inicializada:false)
-//		// variables = [$a]
-//		// maxStack =  1
-//		ret
-		
 		
 		DeclaracionIniciar resEsp = new DeclaracionIniciar(BOOLEAN_VERDADERO_A_TESTEAR, Tipo.BOOLEAN, VARIABLE_A_TESTEAR);
 		String cadena = "poner esVerdadero en boolean $variable2.";
@@ -87,43 +83,84 @@ public class TestCases {
 		assertNotNull(resultado);
 		assertNotNull(resEsp);
 		assertFalse(resEsp.equals(resultado));
+	}  
+	
+	
+	@Test
+	public void testSiEntoncesConDosVariables() throws Exception {
+		System.out.println("{poner 5 en entero $variable. poner 6 en entero $variable2. si $variable < $variable2 entonces poner 8 en entero $variable3.}");
+
+		DeclaracionIniciar variableAChequear = new DeclaracionIniciar(NUMERAL_A_TESTEAR,Tipo.ENTERO,VARIABLE_A_TESTEAR);
+		DeclaracionIniciar variableAChequearSegunda = new DeclaracionIniciar(NUMERAL_A_TESTEAR_SEGUNDA,Tipo.ENTERO,VARIABLE_A_TESTEAR_SEGUNDA);
+		
+		assertNotNull(variableAChequear);
+		assertNotNull(variableAChequearSegunda);
+		
+		//Sentencia son: asignacion, declaración, etc etc		
+		
+		Sentencia [] listaSentencias = new Sentencia[2];		
+		listaSentencias[0] = variableAChequear;
+		listaSentencias[1] = variableAChequearSegunda;
+		Secuencia unaSecuencia = new Secuencia(listaSentencias);
+				
+        boolean variableBooleana = (variableAChequear < variableAChequearSegunda)? true:false;
+		
+		Expresion exp = new Expresion()
+//		SiEntonces()
+		
+//		public SiEntonces(Expresion condition, Sentencia thenBody) {
+//			this.condition = condition;
+//			this.thenBody = thenBody;
+//		}
+				
+		DeclaracionIniciar resEsp = new DeclaracionIniciar(BOOLEAN_VERDADERO_A_TESTEAR, Tipo.BOOLEAN, VARIABLE_A_TESTEAR);
+		String sentencia = "{poner 5 en entero $variable. poner 6 en entero $variable2. si $variable < $variable2 entonces poner 8 en entero $variable3.}";
+		DeclaracionIniciar resultado = (DeclaracionIniciar)Parser.parse(sentencia).value;
+
+		assertNotNull(resultado);
+		assertNotNull(resEsp);
+		assertFalse(resEsp.equals(resultado));
 	}
-//	poner 1 en entero $variable1. 
-//	poner 6 en entero $variable2. 
-//
-//
-//	si $variable1 < $variable2 entonces
-//	poner $variable1 + 1 en $variable1.  
+
+
+	
+
 	
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
-//	Bienvenidos al compilador de EASY_LANGUAGE!
-//
-//	> {poner 5 en entero $a. poner 6 en entero $b. si $a<$b entonces poner 8 en entero $c.}
+//	> {poner esFalso en boolean $a. si $a entonces poner 5 en entero $b.}
 //
 //	=============
 //	=============
-//	key :$a (tipo:entero inicializada:true)
+//	key :$a (tipo:boolean inicializada:true)
 //	key :$b (tipo:entero inicializada:true)
-//	key :$c (tipo:entero inicializada:true)
-//	// variables = [$a, $b, $c]
-//	// maxStack =  2
+	
+//	// variables = [$a, $b]
+//	// maxStack =  1
 //	ldloc 0
-//	ldloc 1
-//	cgt 
-//	ldc.i4.0 
-//	ceq 
 //	brsfalse IL_0
 //	IL_0:nop
 //	ret
-	
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//{ crearVariable $a tipo boolean. crearVariable $b tipo boolean. poner esVerdadero en $a. poner esFalso en $b. si $a y $b entonces nada. }
+//
+//{ crearLista $l1 tipo entero cantidad 3. poner 4 en $l1 posicion 1. poner 3 en $l1 posicion 2. poner 8 en $l1 posicion 3. }
+//
+//{ crearLista $l1 tipo entero cantidad 3. poner 4 en $l1 posicion 0. poner 3 en $l1 posicion 1. poner 8 en $l1 posicion 2. }
+//
+//{ crearLista $l1 tipo entero cantidad 3. poner 4 en $l1 posicion -0. poner 3 en $l1 posicion -1. poner 8 en $l1 posicion -2. }
 }
